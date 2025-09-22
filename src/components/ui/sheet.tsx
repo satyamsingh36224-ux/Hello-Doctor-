a42bd614e15f7ff2444ebf4ebf276572e6eb3c2a
Satyam Singh <satyamsingh36224@gmail.com>
2025-09-22 16:59:35 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -69,6 +69,7 @@ const SheetContent = React.forwardRef<
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
+       <SheetPrimitive.Title className="sr-only">Sheet</SheetPrimitive.Title>
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
